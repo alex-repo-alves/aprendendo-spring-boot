c662dbcbaf9383a6f9e0134d95eccfe94f92a0d1
Alex Alves <daoraalex@outlook.com>
2023-02-20 17:23:35 -0300

    Initial commit

---
+++ curso-api-rest-documentada-spring-boot-dio/src/main/java/dev/alex/cursoapirestdocumentadaspringbootdio/CursoApiRestDocumentadaSpringBootDioApplication.java
@@ -0,0 +1,13 @@
+package dev.alex.cursoapirestdocumentadaspringbootdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApiRestDocumentadaSpringBootDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApiRestDocumentadaSpringBootDioApplication.class, args);
+	}
+
+}
---
+++ curso-api-rest-documentada-spring-boot-dio/src/test/java/dev/alex/cursoapirestdocumentadaspringbootdio/CursoApiRestDocumentadaSpringBootDioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alex.cursoapirestdocumentadaspringbootdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiRestDocumentadaSpringBootDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
